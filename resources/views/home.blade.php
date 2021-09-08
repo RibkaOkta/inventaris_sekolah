60595220b899491fdc67daa81067829ed8c479c0
RibkaOkta <ribkaekklesia6@gmail.com>
2021-09-08 14:38:58 +0700

    1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Coba git</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+   <h3>Data mahasiswa</h3> 
+   <ul>
+   
+   </ul>
+</body>
+</html>
